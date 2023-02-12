09a7e75f76b0a0e925c3ee6fa8b2acda208b98b6
Omotoye Shamsudeen Adekoya <adekoyaomotoye@gmail.com>
2023-02-13 00:29:35 +0100

    :construction: <tested OK> completed the integration of the phase 1 states with the required server nodes

--- exprob_logic/scripts/modules/states.py
+++ exprob_logic/scripts/modules/states.py
@@ -1,18 +1,23 @@
 import rospy
 import smach  # importing the library for the creation of the state machine
 
-from typing import List, Final, Any
+from typing import List, Final, Any, Optional
+from enum import Enum
 
 # Brings in the SimpleActionClient
 
 # Here we bring in all the messages required to interface with each of the nodes
+from exprob_msgs.srv import Knowledge, KnowledgeResponse, KnowledgeRequest  # type: ignore# armor custom messages
+
+BATTERY_LOW: bool = False
+STOP_CALL: bool = False
 
 
 ###***********   PHASE 1   ***********************#####
 
 
 class CheckMap(smach.State):
-    def __init__(self):
+    def __init__(self) -> None:
         smach.State.__init__(
             self,
             outcomes=[
@@ -25,25 +30,24 @@ class CheckMap(smach.State):
             output_keys=[],
             input_keys=[],
         )
+        self.ret: str = ""
 
     def execute(self, userdata: Any) -> str:
-        result: List[bool] = [False, False, False, False, True]
-        ret: str = ""
-        if result[0]:
-            ret = "no map exist"
-        elif result[1]:
-            ret = "map available"
-        elif result[2]:
-            ret = "map check failed"
-        elif result[3]:
-            ret = "battery low"
-        elif result[4]:
-            ret = "stop call"
-        return ret
+        if rospy.has_param("/topological_map"):
+            self.ret = (
+                "map available"
+                if len(rospy.get_param("/topological_map"))
+                else "no map exist"
+            )
+        else:
+            self.ret = "map check failed"
+        self.ret = "battery low" if BATTERY_LOW else self.ret
+        self.ret = "stop call" if STOP_CALL else self.ret
+        return self.ret
 
 
 class BuildMap(smach.State):
-    def __init__(self):
+    def __init__(self) -> None:
         smach.State.__init__(
             self,
             outcomes=[
@@ -55,42 +59,60 @@ class BuildMap(smach.State):
             output_keys=[],
             input_keys=[],
         )
+        self.ret: str = ""
 
     def execute(self, userdata: Any) -> str:
-        result: List[bool] = [True, False, False, False, False]
-        ret: str = ""
-        if result[0]:
-            ret = "mapping completed"
-        elif result[1]:
-            ret = "mapping failed"
-        elif result[2]:
-            ret = "stop call"
-        elif result[3]:
-            ret = "battery low"
-        return ret
+        #   NOTE: This state would never be visited for this stage of the project
+        #   it is just a place holder for when the map would actually be required
+        #   to be built in a more complicated version of this project.
+        self.ret = "battery low" if BATTERY_LOW else self.ret
+        self.ret = "stop call" if STOP_CALL else self.ret
+        return self.ret
 
 
 class UpdateKnowledge(smach.State):
-    def __init__(self):
+    def __init__(self) -> None:
         smach.State.__init__(
             self,
             outcomes=["knowledge updated", "update failed", "stop call", "battery low"],
             output_keys=[],
             input_keys=[],
         )
+        self.ret: str = ""
+
+    def call_knowledge_srv(self) -> Optional[bool]:
+        try:
+            rospy.wait_for_service("/knowledge_srv", 5)
+            knowledge_srv: rospy.ServiceProxy = rospy.ServiceProxy(
+                "/knowledge_srv", Knowledge
+            )
+            request: KnowledgeRequest = KnowledgeRequest()
+            request.goal = "update topology"
+            result: KnowledgeResponse = knowledge_srv(request)
+
+            if result.result == "updated":
+                rospy.loginfo(
+                    "The Topological Map has been successfully updated into the OWL Ontology"
+                )
+                return True
+            else:
+                rospy.loginfo(
+                    "The knowledge server failed to update the Ontology with the Topological Map"
+                )
+                return False
+        except rospy.ServiceException as e:
+            rospy.logerr("Service call failed: {e}".format(e=e))
+        except rospy.ROSException as e:
+            rospy.logerr(f"ROS Exception: {e}")
+        except Exception as e:
+            rospy.logerr(e)
+        return None
 
     def execute(self, userdata: Any) -> str:
-        result: List[bool] = [True, False, False, False, False]
-        ret: str = ""
-        if result[0]:
-            ret = "Knowledge updated"
-        elif result[1]:
-            ret = "update failed"
-        elif result[2]:
-            ret = "stop call"
-        elif result[3]:
-            ret = "battery low"
-        return ret
+        self.ret = "knowledge updated" if self.call_knowledge_srv() else "update failed"
+        self.ret = "battery low" if BATTERY_LOW else self.ret
+        self.ret = "stop call" if STOP_CALL else self.ret
+        return self.ret
 
 
 ###***********   PHASE 2   ***********************#####
@@ -198,8 +220,8 @@ class GetNextPointOfInterest(smach.State):
         smach.State.__init__(
             self,
             outcomes=[
-                "reachable urgency room", 
-                "no reachable urgency room", 
+                "reachable urgency room",
+                "no reachable urgency room",
                 "no reachable corridor",
                 "query failed",
                 "battery low",
--- exprob_logic/scripts/state_machine.py
+++ exprob_logic/scripts/state_machine.py
@@ -10,176 +10,179 @@ import smach_ros
 from modules.states import *
 
 if __name__ == "__main__":
-    rospy.init_node("state_machine")
+    try:
+        rospy.init_node("state_machine")
 
-    # Create a SMACH state machine object
-    surveillance: smach.StateMachine = smach.StateMachine(
-        outcomes=["End of Surveillance"]
-    )
-    # Open the State Machine Container
-    with surveillance:
-        # Creating substates(phases) for the surveillance state machine.
-        phase1: smach.StateMachine = smach.StateMachine(
-            outcomes=["knowledge updated", "battery low", "stop call"]
+        # Create a SMACH state machine object
+        surveillance: smach.StateMachine = smach.StateMachine(
+            outcomes=["End of Surveillance"]
         )
-        phase2: smach.StateMachine = smach.StateMachine(
-            outcomes=["battery low", "stop call"]
-        )
-        phase3: smach.StateMachine = smach.StateMachine(
-            outcomes=["battery charged", "stop call"]
-        )
+        # Open the State Machine Container
+        with surveillance:
+            # Creating substates(phases) for the surveillance state machine.
+            phase1: smach.StateMachine = smach.StateMachine(
+                outcomes=["knowledge updated", "battery low", "stop call"]
+            )
+            phase2: smach.StateMachine = smach.StateMachine(
+                outcomes=["battery low", "stop call"]
+            )
+            phase3: smach.StateMachine = smach.StateMachine(
+                outcomes=["battery charged", "stop call"]
+            )
 
-        # Opening the substate container
-        with phase1:
-            # Add states to the substatemachine container
+            # Opening the substate container
+            with phase1:
+                # Add states to the substatemachine container
+                smach.StateMachine.add(
+                    "Check Map Availability",
+                    CheckMap(),
+                    transitions={
+                        "no map exist": "Build Topological Map",
+                        "map available": "Update Knowledge",
+                        "map check failed": "Check Map Availability",
+                    },
+                    # remapping={"": ""},
+                )
+
+                smach.StateMachine.add(
+                    "Build Topological Map",
+                    BuildMap(),
+                    transitions={
+                        "mapping completed": "Update Knowledge",
+                        "mapping failed": "Build Topological Map",
+                    },
+                    # remapping={"": ""},
+                )
+
+                smach.StateMachine.add(
+                    "Update Knowledge",
+                    UpdateKnowledge(),
+                    transitions={
+                        "update failed": "Update Knowledge",
+                    },
+                    # remapping={"": ""},
+                )
+
             smach.StateMachine.add(
-                "Check Map Availability",
-                CheckMap(),
+                "Phase 1 (Build Knowledge)",
+                phase1,
                 transitions={
-                    "no map exist": "Build Topological Map",
-                    "map available": "Update Knowledge",
-                    "map check failed": "Check Map Availability",
+                    "knowledge updated": "Phase 2 (Surveillance)",
+                    "battery low": "Phase 3 (Battery Recharging)",
+                    "stop call": "Phase 3 (Battery Recharging)",
                 },
                 # remapping={"": ""},
             )
 
+            # Opening the phase 2 substatemachine container
+            with phase2:
+                # Add states to the substatemachine container
+                smach.StateMachine.add(
+                    "Get Next Point of Interest",
+                    GetNextPointOfInterest(),
+                    transitions={
+                        "reachable urgency room": "GoTo Room",
+                        "no reachable urgency room": "GoTo Corridor",
+                        "no reachable corridor": "Survey Corridor",
+                        "query failed": "Get Next Point of Interest",
+                    },
+                    # remapping={"": ""},
+                )
+                smach.StateMachine.add(
+                    "GoTo Room",
+                    GoToRoom(),
+                    transitions={
+                        "at room": "Survey Room",
+                        "failed to reach room": "GoTo Room",
+                        "battery low": "GoTo Corridor",
+                        "stop call": "GoTo Corridor",
+                    },
+                    # remapping={"": ""},
+                )
+                smach.StateMachine.add(
+                    "GoTo Corridor",
+                    GoToCorridor(),
+                    transitions={
+                        "at corridor": "Survey Corridor",
+                        "failed to reach corridor": "GoTo Corridor",
+                    },
+                    # remapping={"": ""},
+                )
+
+                smach.StateMachine.add(
+                    "Survey Room",
+                    SurveyRoom(),
+                    transitions={
+                        "survey completed": "GoTo Corridor",
+                        "survey failed": "Survey Room",
+                        "battery low": "GoTo Corridor",
+                        "stop call": "GoTo Corridor",
+                    },
+                    # remapping={"": ""},
+                )
+                
+                smach.StateMachine.add(
+                    "Survey Corridor",
+                    SurveyCorridor(),
+                    transitions={
+                        "survey completed": "Get Next Point of Interest", 
+                        "survey failed": "Survey Corridor",
+                    },
+                    # remapping={"": ""},
+                )
+
             smach.StateMachine.add(
-                "Build Topological Map",
-                BuildMap(),
+                "Phase 2 (Surveillance)",
+                phase2,
                 transitions={
-                    "mapping completed": "Update Knowledge",
-                    "mapping failed": "Build Topological Map",
+                    "battery low": "Phase 3 (Battery Recharging)", 
+                    "stop call": "Phase 3 (Battery Recharging)",
                 },
                 # remapping={"": ""},
             )
 
+            # Opening the phase 3 substatemachine container
+            with phase3:
+                # Add states to the substatemachine container
+                smach.StateMachine.add(
+                    "GoTo Recharge Point",
+                    GoToRechargePoint(),
+                    transitions={
+                        "at recharge point": "Battery Charging",
+                        "failed to reach charging point": "GoTo Recharge Point",
+                    },
+                    # remapping={"": ""},
+                )
+                smach.StateMachine.add(
+                    "Battery Charging",
+                    BatteryCharging(),
+                    transitions={
+                        "battery charging failed": "Battery Charging",
+                    },
+                    # remapping={"": ""},
+                )
+
             smach.StateMachine.add(
-                "Update Knowledge",
-                UpdateKnowledge(),
+                "Phase 3 (Battery Recharging)",
+                phase3,
                 transitions={
-                    "update failed": "Update Knowledge",
+                    "battery charged": "Phase 2 (Surveillance)",
+                    "stop call": "End of Surveillance",
                 },
                 # remapping={"": ""},
             )
 
-        smach.StateMachine.add(
-            "Phase 1 (Build Knowledge)",
-            phase1,
-            transitions={
-                "knowledge updated": "Phase 2 (Surveillance)",
-                "battery low": "Phase 3 (Battery Recharging)",
-                "stop call": "Phase 3 (Battery Recharging)",
-            },
-            # remapping={"": ""},
-        )
-
-        # Opening the phase 2 substatemachine container
-        with phase2:
-            # Add states to the substatemachine container
-            smach.StateMachine.add(
-                "Get Next Point of Interest",
-                GetNextPointOfInterest(),
-                transitions={
-                    "reachable urgency room": "GoTo Room",
-                    "no reachable urgency room": "GoTo Corridor",
-                    "no reachable corridor": "Survey Corridor",
-                    "query failed": "Get Next Point of Interest",
-                },
-                # remapping={"": ""},
+            # Create and start the introspection server
+            sis = smach_ros.IntrospectionServer(
+                "server_name", surveillance, "Surveillance Robot State Machine"
             )
-            smach.StateMachine.add(
-                "GoTo Room",
-                GoToRoom(),
-                transitions={
-                    "at room": "Survey Room",
-                    "failed to reach room": "GoTo Room",
-                    "battery low": "GoTo Corridor",
-                    "stop call": "GoTo Corridor",
-                },
-                # remapping={"": ""},
-            )
-            smach.StateMachine.add(
-                "GoTo Corridor",
-                GoToCorridor(),
-                transitions={
-                    "at corridor": "Survey Corridor",
-                    "failed to reach corridor": "GoTo Corridor",
-                },
-                # remapping={"": ""},
-            )
+            sis.start()
 
-            smach.StateMachine.add(
-                "Survey Room",
-                SurveyRoom(),
-                transitions={
-                    "survey completed": "GoTo Corridor",
-                    "survey failed": "Survey Room",
-                    "battery low": "GoTo Corridor",
-                    "stop call": "GoTo Corridor",
-                },
-                # remapping={"": ""},
-            )
-            
-            smach.StateMachine.add(
-                "Survey Corridor",
-                SurveyCorridor(),
-                transitions={
-                    "survey completed": "Get Next Point of Interest", 
-                    "survey failed": "Survey Corridor",
-                },
-                # remapping={"": ""},
-            )
+            # Execute SMACH plan
+            outcome = surveillance.execute()
 
-        smach.StateMachine.add(
-            "Phase 2 (Surveillance)",
-            phase2,
-            transitions={
-                "battery low": "Phase 3 (Battery Recharging)", 
-                "stop call": "Phase 3 (Battery Recharging)",
-            },
-            # remapping={"": ""},
-        )
-
-        # Opening the phase 3 substatemachine container
-        with phase3:
-            # Add states to the substatemachine container
-            smach.StateMachine.add(
-                "GoTo Recharge Point",
-                GoToRechargePoint(),
-                transitions={
-                    "at recharge point": "Battery Charging",
-                    "failed to reach charging point": "GoTo Recharge Point",
-                },
-                # remapping={"": ""},
-            )
-            smach.StateMachine.add(
-                "Battery Charging",
-                BatteryCharging(),
-                transitions={
-                    "battery charging failed": "Battery Charging",
-                },
-                # remapping={"": ""},
-            )
-
-        smach.StateMachine.add(
-            "Phase 3 (Battery Recharging)",
-            phase3,
-            transitions={
-                "battery charged": "Phase 2 (Surveillance)",
-                "stop call": "End of Surveillance",
-            },
-            # remapping={"": ""},
-        )
-
-        # Create and start the introspection server
-        sis = smach_ros.IntrospectionServer(
-            "server_name", surveillance, "Surveillance Robot State Machine"
-        )
-        sis.start()
-
-        # Execute SMACH plan
-        outcome = surveillance.execute()
-
-        # Wait for ctrl-c to stop the application
-        rospy.spin()
-        sis.stop()
+            # Wait for ctrl-c to stop the application
+            rospy.spin()
+            sis.stop()
+    except rospy.ROSInterruptException:
+        pass
