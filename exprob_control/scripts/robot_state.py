e0e52b79b30f0540abef1177b0089e6575ae50d6
Omotoye Shamsudeen Adekoya <adekoyaomotoye@gmail.com>
2023-02-12 18:00:47 +0100

    :construction: :package: <init> control package for the robot_state and controller node

---
+++ exprob_control/scripts/robot_state.py
@@ -0,0 +1,10 @@
+import time 
+
+class RobotState:
+    def __init__(self) -> None:
+        self.battery_level: float = 100.0
+        
+    def bettery_manager(self) -> None:
+        pass       
+        
+        
