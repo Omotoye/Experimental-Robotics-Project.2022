721ba95a09fb186d4ad3f5fdd14339b13b7ce484
Omotoye Shamsudeen Adekoya <adekoyaomotoye@gmail.com>
2023-02-22 23:10:36 +0100

    :white_check_mark: :bulb: <docstring> added docstrito the RobotNavigation class and all its methods

--- exprob_navigation/scripts/navigation.py
+++ exprob_navigation/scripts/navigation.py
@@ -24,8 +24,9 @@ from typing import Dict, List, Union
 LocationInfo = Dict[str, Union[float, List[str]]]
 
 
-# color class to highlight log messages.
 class bcolors:
+    """Color clas to highlight log messages."""
+
     HEADER = "\033[95m"
     OKBLUE = "\033[94m"
     OKCYAN = "\033[96m"
@@ -38,6 +39,17 @@ class bcolors:
 
 
 class RobotNavigation:
+    """Navigate the robot to the given point of interest
+
+    This is an action server that takes in a goal point which is dubbed `Point Of Interest (POI)`, the
+    exact coordinate to the POI is taken from the `topological_map` parameter server, for which it does
+    some sort of check to see if the given POI is valid, if not it Aborts the goal.
+        The action server is capable of navigating the robot to a location of any type, either `room`,
+    `corridor`, or `recharge point`. While the action server is navigating the robot to the POI, it is
+    actively checking for preempt request so as to stop the navigation and set the state of the action
+    to preempted and log the preempt action to the terminal.
+    """
+
     # create messages that are used to publish feedback/result
     _feedback: RobotNavFeedback = (
         RobotNavFeedback()
@@ -46,6 +58,14 @@ class RobotNavigation:
     _result: RobotNavResult = RobotNavResult()
 
     def __init__(self, name: str) -> None:
+        """Initializes the Robot Navigation action server
+
+        In here the action server is initialized to listen for goal request from clients, it also
+        Log the success of the initialization onto the terminal
+
+        Args:
+            name (str): this is the name of the node which would then be used to initialize the action name
+        """
         self._action_name: str = name
         self._as: actionlib.SimpleActionServer = actionlib.SimpleActionServer(
             self._action_name,
@@ -54,9 +74,23 @@ class RobotNavigation:
             auto_start=False,
         )
         self._as.start()
-        rospy.loginfo(f"{bcolors.OKCYAN}ROBOT NAVIGATION{bcolors.ENDC}: initialization completed {bcolors.OKGREEN}SUCCESSFULLY{bcolors.ENDC}")
+        rospy.loginfo(
+            f"{bcolors.OKCYAN}ROBOT NAVIGATION{bcolors.ENDC}: initialization completed {bcolors.OKGREEN}SUCCESSFULLY{bcolors.ENDC}"
+        )
 
     def _go_to_poi(self, poi_req: str) -> str:
+        """Navigates the robot to the given point of interest
+
+        It gets the Locations (POI) coordinates from the `topological_map` parameter
+        server and then navigates the robot to the corresponding coordinates of the given
+        POI, which checking if preemption was not requested by the client.
+
+        Args:
+            poi_req (str): the point of interest that the robot should be navigated to
+
+        Returns:
+            str: the result of the navigation, either `goal preempted` or `goal reached`
+        """
         # get the coordinate corresponding to the point of interest given
         goal_info: LocationInfo = rospy.get_param(f"/topological_map/{poi_req}")
         self._result.x_cord = goal_info["x_axis"]
@@ -77,6 +111,17 @@ class RobotNavigation:
         return "goal reached"
 
     def _check_preempt(self, poi: str) -> bool:
+        """Checks if preemtion has been requested by the Client
+
+        If Preemption is requested it returns `True` and logs the status to
+        the terminal otherwise returns `False`
+
+        Args:
+            poi (str): the point of interest, used for logging if preempt was requested
+
+        Returns:
+            bool: `True` if preempt request `False` otherwise
+        """
         if self._as.is_preempt_requested():
             rospy.loginfo(
                 f"{bcolors.OKCYAN}NAVIGATION{bcolors.ENDC}: to {poi} "
@@ -88,9 +133,18 @@ class RobotNavigation:
         return False
 
     def _handle_feedback(self) -> None:
-        pass  # this would be implemented in later versions
+        """This would be implemented in later versions of the project"""
+        pass
 
     def execute_cb(self, goal: RobotNavGoal) -> None:
+        """Executes the action goal request to the robot_navigation action server
+
+        This is the callback function that is called whenever the robot_navigation
+        action server is sent a goal request
+
+        Args:
+            goal (RobotNavGoal): The goal request messege sent to the server from the client
+        """
         # Checking if the Point of Interest exists
         if rospy.has_param(f"/topological_map/{goal.poi}"):
             result: str = self._go_to_poi(goal.poi)
